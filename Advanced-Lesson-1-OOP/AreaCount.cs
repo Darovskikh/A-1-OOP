154677477d4e47b34f69dde4220b7c1f00f9866e
Владислав Даровских <vitebsk.public@gmail.com>
2019-04-30 19:52:20 +0300

    Task

---
+++ Advanced-Lesson-1-OOP/AreaCount.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_Lesson_1_OOP
+{
+    interface AreaCount
+    {
+        void CalculateArea();
+    }
+}
---
+++ Advanced-Lesson-1-OOP/Triangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_Lesson_1_OOP
+{
+    class Triangle : AreaCount
+    {
+        public double Height { get; }
+        public double Base { get; }
+        public double Area { get; private set; }
+
+        public Triangle(double height, double @base)
+        {
+            Height = height;
+            Base = @base;
+            CalculateArea();
+        }
+        public void CalculateArea()
+        {
+            Area = 0.5 * Base * Height;
+        }
+    }
+}
